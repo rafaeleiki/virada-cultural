19aead6ada606014606a7f566dc94a74ee18260c
Rafael <rafaeleiki@yahoo.com.br>
2016-11-12 22:21:21 -0200

    Refatoração

--- js/os.js
+++ js/os.js
@@ -1,11 +1,17 @@
 (function() {
-    const prefix = window.location.pathname.indexOf('index.html') >= 0? '' : '../';
-    const cssLink = prefix + 'ratchet-2.0.2/dist/css/ratchet-theme-#os#.css';
     var operationalSystem = localStorage.getItem('os');
+
     if (['ios', 'android'].indexOf(operationalSystem) >= 0) {
+        document.head.appendChild(createOsStylesheetLink(operationalSystem));
+    }
+
+    function createOsStylesheetLink(operationalSystem) {
+        const prefix = window.location.pathname.indexOf('index.html') >= 0? '' : '../';
+        const cssLink = prefix + 'ratchet-2.0.2/dist/css/ratchet-theme-#os#.css';
+
         var link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = cssLink.replace('#os#', operationalSystem);
-        document.head.appendChild(link);
+        return link;
     }
 })();
