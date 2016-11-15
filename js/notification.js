c49ed8c2a8f5f3aa5f782f97ab7fa33b10a30928
Rafael <rafaeleiki@yahoo.com.br>
2016-11-15 15:19:36 -0200

    Criado código para notificações

---
+++ js/notification.js
@@ -0,0 +1,31 @@
+var Notification = (function() {
+
+
+    function $(id) {
+        return document.getElementById(id);
+    }
+
+    var notification = {
+        show: function (message) {
+            document.body.appendChild(this.createNotification(message));
+        },
+        createNotification: function (message) {
+            var notificationBlock = document.createElement('div');
+            notificationBlock.className = 'notification';
+            notificationBlock.innerHTML = message;
+
+            function listener() {
+                document.body.removeChild(notificationBlock);
+                notificationBlock.removeEventListener('webkitAnimationEnd', this);
+                notificationBlock.removeEventListener('click', this);
+            }
+
+            notificationBlock.addEventListener('click', listener);
+            notificationBlock.addEventListener('webkitAnimationEnd', listener);
+
+            return notificationBlock;
+        }
+    };
+
+    return notification;
+})();
